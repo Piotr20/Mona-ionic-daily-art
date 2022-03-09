2f03818d0a0f1418dc50422c59650bf07ce31a18
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2022-03-09 22:33:56 +0100

    init

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzm5xiuCl6cEF18lL6GKvDcHraN-Vcnis",
+  authDomain: "mona-daily-art.firebaseapp.com",
+  projectId: "mona-daily-art",
+  storageBucket: "mona-daily-art.appspot.com",
+  messagingSenderId: "845182368400",
+  appId: "1:845182368400:web:8fbcd41a067d4d484d3615",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
